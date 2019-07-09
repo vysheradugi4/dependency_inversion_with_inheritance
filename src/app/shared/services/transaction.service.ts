121132931a57983ff94b75e6f970e2cfe28fccf7
danisimov <vysheradugi4@gmail.com>
2019-07-09 11:37:49 +0300

    Fixed typo

--- src/app/shared/services/transaction.service.ts
+++ src/app/shared/services/transaction.service.ts
@@ -2,7 +2,7 @@ import { Transaction } from 'src/app/shared/models/transaction.model';
 import { Service } from 'src/app/shared/services/service';
 
 
-export class TransactionService implements Service<Transaction> {
+export class TransactionService extends Service<Transaction> {
   public delete(transaction: Transaction) {
     console.log('Deleted from Transaction service!', transaction);
   }
